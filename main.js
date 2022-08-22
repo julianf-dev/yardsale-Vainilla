d68ad212b20405f6fccdc214814368d983c4e68e
JulianF <juliandavidbf@gmail.com>
2022-08-22 07:51:57 -0400

    @feat: Update styles

--- main.js
+++ main.js
@@ -6,49 +6,50 @@ const handleMenu = document.querySelector(".menu-navbar");
 const shoppingCart = document.querySelector(".navbar-shopping-cart");
 const mainSection = document.querySelector(".main")
 const shoppingArrow = document.querySelector(".shopping-arrow");
+const cardsContainer = document.querySelector('.cards-container');
+
 
 /*  Inactives class */
 
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".menu-mobile ");
-const shoppingSection = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector(".shopping");
+const productDetailContainer = document.querySelector(".product-detail");
+const productCartContainer = document.querySelector(".product-card");
 
 /* Valid is Open*/
 
 const toggleMenu = () => {
-    shoppingSection.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
     desktopMenu.classList.toggle("inactive");
 };
 
 const toggleMenuMobile = () => {
     mobileMenu.classList.toggle("inactive");
-    shoppingSection.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 };
 
 function toggleCarritoAside() {
     desktopMenu.classList.add("inactive");
     mobileMenu.classList.add("inactive");
-    shoppingSection.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 function closeAllMenus(){
     /* Tener en cuenta el scope */
-    const isSectionOpen =  shoppingSection.classList.contains('inactive');
-    console.log(isSectionOpen)
+    const isSectionClosed =  shoppingCartContainer.classList.contains('inactive');
+    const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
+
     if(!desktopMenu.classList.contains('inactive')){
         desktopMenu.classList.add("inactive");
     }
-    if(!isSectionOpen){
-        console.log(isSectionOpen)
-        shoppingSection.classList.add('inactive')
+    if(!isSectionClosed){
+        shoppingCartContainer.classList.add('inactive')
+    }
+    if(!isMenuMobileClosed){
+        mobileMenu.classList.add('inactive')
     }
 }
-
-arrowMenu.addEventListener("click", toggleMenu);
-handleMenu.addEventListener("click", toggleMenuMobile);
-shoppingCart.addEventListener("click", toggleCarritoAside);
-shoppingArrow.addEventListener("click", toggleCarritoAside);
-mainSection.addEventListener("click", closeAllMenus);
 
 function productCard( 
     name,
@@ -87,6 +88,10 @@ products.map(( { name, src, price}) => {
     paintProducts.push(productCard(name, src, price))
 })
 
-console.log(paintProducts)
-const cardsContainer = document.querySelector('.cards-container');
 cardsContainer.append(...paintProducts)
+
+arrowMenu.addEventListener("click", toggleMenu);
+handleMenu.addEventListener("click", toggleMenuMobile);
+shoppingCart.addEventListener("click", toggleCarritoAside);
+shoppingArrow.addEventListener("click", toggleCarritoAside);
+mainSection.addEventListener("click", closeAllMenus);
